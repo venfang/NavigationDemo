8987ee4f2bfe3d50318640b32c0cacde3d440e5f
venfang <ewevenfang@gmail.com>
2021-07-15 16:55:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/navigationdemo/LoginFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.navigationdemo.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.btnLogin.setOnClickListener() {
+            val inputUserName: String = binding.tfName.text.toString()
+            val inputPassword: String = binding.tfPassword.text.toString()
+            if (inputPassword == "123" && inputUserName == "abc") {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_loginFragment_to_questionFragment)
+            } else {
+                Toast.makeText(context, "invalid User name or password!", Toast.LENGTH_LONG).show()
+
+            }
+        }
+        return binding.root
+    }
+
+}
